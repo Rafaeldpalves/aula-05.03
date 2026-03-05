31f126371954365176ef1ae1bd47f3d835953ee8
rafaeldpalves <rafaeldospassosalves217@gmail.com>
2026-03-05 20:12:27 -0300

    exercicio 3

--- ex3.c
+++ ex3.c
@@ -4,18 +4,13 @@
 int main() {
 
     int *protocolo_urgente;
-
-    
     protocolo_urgente = (int*) malloc(sizeof(int));
 
-    
     *protocolo_urgente = 999;
 
-   
     printf("Valor do protocolo urgente: %d\n", *protocolo_urgente);
 
-    
-    free(protocolo_urgente);
+     free(protocolo_urgente);
 
     return 0;
 }
