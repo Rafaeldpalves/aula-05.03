8b128100ec2c0ac00cb21e101f546504072b562b
rafaeldpalves <rafaeldospassosalves217@gmail.com>
2026-03-05 20:10:36 -0300

    exercicio 3

--- ex3.c
+++ ex3.c
@@ -5,16 +5,16 @@ int main() {
 
     int *protocolo_urgente;
 
-    // Alocando memória dinamicamente para um inteiro
+    
     protocolo_urgente = (int*) malloc(sizeof(int));
 
-    // Guardando o valor no espaço alocado
+    
     *protocolo_urgente = 999;
 
-    // Imprimindo o valor
+   
     printf("Valor do protocolo urgente: %d\n", *protocolo_urgente);
 
-    // Liberando a memória
+    
     free(protocolo_urgente);
 
     return 0;
